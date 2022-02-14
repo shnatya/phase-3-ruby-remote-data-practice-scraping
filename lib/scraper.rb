19d28aaf448fc60db84eac09b70d74edbf08ff60
Anastasia Shatokhina <shnatya@gmail.com>
2022-02-13 16:47:54 -0800

    commit tree f46cfd7884bd99fdd66934759329a3a1ea981531

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = open("https://flatironschool.com/") #We can use the following line to grab the HTML
+doc = Nokogiri::HTML(html) #convert the string of html into a NodeSet (aka, a bunch of nested "nodes") that we can easily play around with.
+
+puts doc
